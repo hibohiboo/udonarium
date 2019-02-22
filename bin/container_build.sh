f6b1ba5d8a8aa1df71c95ea640f8561510c72a02
hibohiboo <swdice66@gmail.com>
2019-02-23 00:53:53 +0900

    dockerで動作確認更新

--- bin/container_build.sh
+++ bin/container_build.sh
@@ -2,5 +2,6 @@
 
 # このシェルスクリプトのディレクトリの絶対パスを取得。
 bin_dir=$(cd $(dirname $0) && pwd)
-
-cd $bin_dir/../docker && docker-compose build
+parent_dir=$bin_dir/..
+cp $parent_dir/package.json $parent_dir/docker/node/package.json
+cd $parent_dir/docker && docker-compose build
