f68b0da86f44730929054ba2001e95164c4c190b
hibohiboo <swdice66@gmail.com>
2021-02-13 00:41:30 +0900

    fix lint

--- src/app/plugins/card-self-hide/cardMenu.ts
+++ src/app/plugins/card-self-hide/cardMenu.ts
@@ -1,71 +1,85 @@
-import { Network } from "@udonarium/core/system";
-import { PresetSound, SoundEffect } from "@udonarium/sound-effect";
-import { ContextMenuSeparator } from "service/context-menu.service";
+import { Network } from '@udonarium/core/system'
+import { PresetSound, SoundEffect } from '@udonarium/sound-effect'
+import { ContextMenuSeparator } from 'service/context-menu.service'
 
 export const createCardMenues = (that) => {
-  const menues = [];
-  if(!that.isVisible || that.isHand){
+  const menues = []
+  if (!that.isVisible || that.isHand) {
     menues.push({
-      name: '表にする', action: () => {
-        that.card.faceUp();
-        SoundEffect.play(PresetSound.cardDraw);
-      }});
-  }else{
+      name: '表にする',
+      action: () => {
+        that.card.faceUp()
+        SoundEffect.play(PresetSound.cardDraw)
+      },
+    })
+  } else {
     menues.push({
-      name: '裏にする', action: () => {
-        that.card.faceDown();
-        SoundEffect.play(PresetSound.cardDraw);
-      }
-    });
+      name: '裏にする',
+      action: () => {
+        that.card.faceDown()
+        SoundEffect.play(PresetSound.cardDraw)
+      },
+    })
   }
-  if(that.isHand){
+  if (that.isHand) {
     menues.push({
-      name: '裏にする', action: () => {
-        that.card.faceDown();
-        SoundEffect.play(PresetSound.cardDraw);
-      }
-    });
-  }else{
+      name: '裏にする',
+      action: () => {
+        that.card.faceDown()
+        SoundEffect.play(PresetSound.cardDraw)
+      },
+    })
+  } else {
     menues.push({
-      name: '自分だけ見る', action: () => {
-        SoundEffect.play(PresetSound.cardDraw);
-        that.card.faceDown();
-        that.owner = Network.peerContext.userId;
-      }
-    });
+      name: '自分だけ見る',
+      action: () => {
+        SoundEffect.play(PresetSound.cardDraw)
+        that.card.faceDown()
+        that.owner = Network.peerContext.userId
+      },
+    })
   }
-  if(!that.isHand){
+  if (!that.isVisible && !that.isHand) {
     menues.push({
-      name: '自分だけ隠す', action: () => {
-        SoundEffect.play(PresetSound.cardDraw);
-        that.card.setSelfHide();
-      }
-    });
+      name: '自分だけ隠す',
+      action: () => {
+        SoundEffect.play(PresetSound.cardDraw)
+        that.card.setSelfHide()
+      },
+    })
   }
 
   menues.push(ContextMenuSeparator)
   menues.push({
-    name: '重なったカードで山札を作る', action: () => {
-      that.createStack();
-      SoundEffect.play(PresetSound.cardPut);
-    }
+    name: '重なったカードで山札を作る',
+    action: () => {
+      that.createStack()
+      SoundEffect.play(PresetSound.cardPut)
+    },
   })
   menues.push(ContextMenuSeparator)
-  menues.push({ name: 'カードを編集', action: () => { that.showDetail(that.card); } })
   menues.push({
-    name: 'コピーを作る', action: () => {
-      let cloneObject = that.card.clone();
-      cloneObject.location.x += that.gridSize;
-      cloneObject.location.y += that.gridSize;
-      cloneObject.toTopmost();
-      SoundEffect.play(PresetSound.cardPut);
-    }
+    name: 'カードを編集',
+    action: () => {
+      that.showDetail(that.card)
+    },
   })
   menues.push({
-    name: '削除する', action: () => {
-      that.card.destroy();
-      SoundEffect.play(PresetSound.sweep);
-    }
+    name: 'コピーを作る',
+    action: () => {
+      const cloneObject = that.card.clone()
+      cloneObject.location.x += that.gridSize
+      cloneObject.location.y += that.gridSize
+      cloneObject.toTopmost()
+      SoundEffect.play(PresetSound.cardPut)
+    },
+  })
+  menues.push({
+    name: '削除する',
+    action: () => {
+      that.card.destroy()
+      SoundEffect.play(PresetSound.sweep)
+    },
   })
   return menues
 }
--- src/app/plugins/card-self-hide/index.ts
+++ src/app/plugins/card-self-hide/index.ts
@@ -1,9 +1,13 @@
-import { PointerData } from "service/pointer-device.service";
-import { createCardMenues } from "./cardMenu";
+import { PointerData } from 'service/pointer-device.service'
+import { createCardMenues } from './cardMenu'
 
 export default {
   cardComponentOnContextMenuHook: (that, position: PointerData) => {
-    const menues = createCardMenues(that);
-    that.contextMenuService.open(position, menues, that.isVisible ? that.name : 'カード');
-  }
+    const menues = createCardMenues(that)
+    that.contextMenuService.open(
+      position,
+      menues,
+      that.isVisible ? that.name : 'カード',
+    )
+  },
 }
