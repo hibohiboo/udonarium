2ca743513ae9d68cc90cb137d6380e15c274e0d0
hibohiboo <swdice66@gmail.com>
2022-11-16 00:57:02 +0900

    add cdk

---
+++ cdk/bin/cdk.ts
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+import 'source-map-support/register'
+import * as cdk from 'aws-cdk-lib'
+import * as dotenv from 'dotenv'
+import { AWSRoopeRUdonaruimStack } from '../lib/cdk-stack'
+
+dotenv.config()
+const envList = [
+  'PROJECT_ID',
+  'BUCKET_NAME',
+  'DISTRIBUTION_ID',
+  'TAG_PROJECT_NAME',
+  'DOMAIN_NAME',
+] as const
+for (const key of envList) {
+  if (!process.env[key]) throw new Error(`please add ${key} to .env`)
+}
+const processEnv = process.env as Record<typeof envList[number], string>
+
+const app = new cdk.App()
+const env = {
+  account: process.env.CDK_DEFAULT_ACCOUNT,
+  region: process.env.CDK_DEFAULT_REGION,
+}
+const projectId = processEnv.PROJECT_ID
+
+new AWSRoopeRUdonaruimStack(app, `${projectId}-stack`, {
+  bucketName: processEnv.BUCKET_NAME,
+  projectId: `${projectId}`,
+  distributionId: processEnv.DISTRIBUTION_ID,
+  domainName: processEnv.DOMAIN_NAME,
+  projectNameTag: processEnv.TAG_PROJECT_NAME,
+  env,
+})
