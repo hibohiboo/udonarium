7f6d6d11b52487fda8ecb5cf6071243800a91895
hibohiboo <swdice66@gmail.com>
2023-01-07 21:25:04 +0900

    fix TODO コメント AB#306

--- src/plugins/object-rotate-off/extends/menu.ts
+++ src/plugins/object-rotate-off/extends/menu.ts
@@ -2,6 +2,8 @@ import { PresetSound, SoundEffect } from "@udonarium/sound-effect";
 import { ContextMenuSeparator } from "service/context-menu.service";
 import { pluginConfig } from "src/plugins/config";
 
+// TODO: ダイス、カード、デッキはcomponentsフォルダに作成せず、直接 import している。 どちらかに統一したい。
+
 export const rotateOffIndividuallyContextMenu = (that:any) => pluginConfig.isOffObjectRotateIndividually ? [
     ContextMenuSeparator,
     getOffMenu(that)
@@ -24,3 +26,4 @@ const getOffMenu = (that:any) => {
     }
   }
 }
+
