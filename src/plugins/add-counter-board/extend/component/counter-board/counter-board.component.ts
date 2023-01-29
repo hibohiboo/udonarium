a8de59fe0a75ae33550702d0919abd797593b968
hibohiboo <swdice66@gmail.com>
2023-01-29 22:22:02 +0900

    fix X方向に進む AB#342

--- src/plugins/add-counter-board/extend/component/counter-board/counter-board.component.ts
+++ src/plugins/add-counter-board/extend/component/counter-board/counter-board.component.ts
@@ -6,6 +6,7 @@ import { TabletopService } from 'service/tabletop.service';
 import { ContextMenuService, ContextMenuAction } from 'service/context-menu.service';
 import { PointerDeviceService } from 'service/pointer-device.service';
 import { TabletopActionService } from 'service/tabletop-action.service';
+import { CoordinateService } from 'service/coordinate.service';
 
 @Component({
   selector: 'counter-board',
@@ -21,6 +22,7 @@ export class CounterBoardComponent implements OnInit, OnDestroy {
     private contextMenuService: ContextMenuService,
     private pointerDeviceService: PointerDeviceService,
     private tabletopActionService: TabletopActionService,
+    private coordinateService: CoordinateService,
   ) { }
 
   ngOnInit() {
@@ -34,30 +36,63 @@ export class CounterBoardComponent implements OnInit, OnDestroy {
     return this.tabletopService.terrains.map(obj=> {
      const countElement = obj.detailDataElement.getFirstElementByName('カウント');
      const count = Number(countElement.value);
-     console.log('countElement.value',countElement.value)
      return {
               name: obj.name
             , count
-            , increaseCount() {
-                const newValue = count + 1;
-
-                countElement.value = `${newValue}`
-                updatePosition(obj, newValue, size);
-              }
-            , decreaseCount() {
-              const newValue = count - 1;
-              countElement.value = `${newValue}`;
-              updatePosition(obj, newValue, size);
-            }
+            , obj
           }
     })
   }
+
+  increaseCount(obj) {
+    const countElement = obj.detailDataElement.getFirstElementByName('カウント');
+    const count = Number(countElement.value);
+    const newValue = count + 1;
+    countElement.value = `${newValue}`;
+    updatePosition(obj, newValue, this.size, this);
+  }
+
+  decreaseCount(obj) {
+    const countElement = obj.detailDataElement.getFirstElementByName('カウント');
+    const count = Number(countElement.value);
+    const newValue = count - 1;
+    countElement.value = `${newValue}`;
+    updatePosition(obj, newValue, this.size, this);
+  }
+
 }
 
-const updatePosition  = (obj, count, size) => {
-  console.log('coutn', count);
-  console.log('size',size)
-  obj.location.x = (count - 1) * size;
-  console.log('update', obj);
+const updatePosition  = (obj, count, size, that) => {
+  // 上に積まれているオブジェクトを取得
+  const currentPositionObjects = that.tabletopService.terrains
+    .filter(item => item.detailDataElement.getFirstElementByName('周囲カウンター')
+                 && item.location.x === obj.location.x
+                 && item.location.y === obj.location.y
+                 && item.posZ > obj.posZ);
+
+  const x = (count - 1) * size; // 0 スタートにするために1を引く
+  const y = obj.location.y;
+
+  // 移動先の一番上のオブジェクトを取得
+  const [topObject] = that.tabletopService.terrains
+  .filter(item => item.detailDataElement.getFirstElementByName('周囲カウンター')
+               && item.location.x === x
+               && item.location.y === y
+  ).sort((a,b)=> b.posZ - a.posZ);
+
+  obj.location.x = x;
+
+  obj.posZ = topObject == null ? 0 : calcNextHeight(topObject, size);
   obj.update();
+  const objHeight = obj.commonDataElement.getFirstElementByName('height').value;
+  const height = size * Number(objHeight)
+  currentPositionObjects.forEach(item=>{
+    item.posZ = item.posZ - height;
+    item.update();
+  });
+}
+
+const calcNextHeight = (topObject, size) => {
+  const topHeight = topObject.commonDataElement.getFirstElementByName('height').value;
+  return Number(topObject.posZ) + Number(topHeight) * size
 }
