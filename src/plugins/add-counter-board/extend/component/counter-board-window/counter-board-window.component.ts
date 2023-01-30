d5107bfb83750eea66640e563c24eb95ad457a5c
hibohiboo <swdice66@gmail.com>
2023-01-31 01:04:31 +0900

    fix 未使用の変数削除

--- src/plugins/add-counter-board/extend/component/counter-board-window/counter-board-window.component.ts
+++ src/plugins/add-counter-board/extend/component/counter-board-window/counter-board-window.component.ts
@@ -1,16 +1,8 @@
-import { Component, OnDestroy, OnInit,HostListener, Input, } from '@angular/core';
-import { EventSystem } from '@udonarium/core/system';
-import { ModalService } from 'service/modal.service';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { PanelService } from 'service/panel.service';
-import { TabletopService } from 'service/tabletop.service';
-import { ContextMenuService, ContextMenuAction } from 'service/context-menu.service';
-import { PointerDeviceService } from 'service/pointer-device.service';
-import { TabletopActionService } from 'service/tabletop-action.service';
-import { CoordinateService } from 'service/coordinate.service';
 import { CounterBoard } from '../../class/counter-board';
 import { CounterBoardService } from '../../service/counter-board.service'
 
-const DETAIL_COUNT_NAME = 'カウント'
 
 @Component({
   selector: 'counter-board-window',
@@ -30,12 +22,6 @@ export class CounterBoardWindowComponent implements OnInit, OnDestroy {
   };
   constructor(
     private panelService: PanelService,
-    private modalService: ModalService,
-    private tabletopService: TabletopService,
-    private contextMenuService: ContextMenuService,
-    private pointerDeviceService: PointerDeviceService,
-    private tabletopActionService: TabletopActionService,
-    private coordinateService: CoordinateService,
     private counterBoardService: CounterBoardService,
   ) { }
 
