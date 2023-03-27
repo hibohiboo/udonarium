f43ee49b854d75528d233a6db4bc1aaf9347e5a0
hibohiboo <swdice66@gmail.com>
2023-03-27 22:05:00 +0900

    fix Zipを入れた時に増える

--- src/plugins/first-fetch-zip-room/extend/app.component.ts
+++ src/plugins/first-fetch-zip-room/extend/app.component.ts
@@ -1,12 +1,19 @@
 import { FileArchiver } from "@udonarium/core/file-storage/file-archiver";
 import { configParam, pluginConfig } from "src/plugins/config";
+import { FileArchiverEx } from "../class/core/file-storage/file-archiver";
+
+const getZipName = ()=>{
+  if(configParam.firstFetchZipRoom) return configParam.firstFetchZipRoom;
+  if(pluginConfig.isAddCounterBoard) return 'number_table';
+  return 'rooper';
+}
 
 export const fetchZipRoom = () => {
   if (!pluginConfig.isFirstFetchZipRoom && !pluginConfig.isAddCounterBoard) return;
-  const zipName = configParam.firstFetchZipRoom || pluginConfig.isAddCounterBoard ? 'number_table': 'rooper';
+  const zipName = getZipName()
   setTimeout(async () => {
     const res = await fetch(`./assets/rooms/${zipName}.zip`);
     const blob = await res.blob();
-    FileArchiver.instance.load([new File([blob], '')]);
+    FileArchiverEx.instance.load([new File([blob], '')]);
   }, 0);
 }
