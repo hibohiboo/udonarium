5f0036ad27f180261d013017c45673cf8bb8917e
hibohiboo <hibohiboo66+github@gmail.com>
2023-10-03 00:24:50 +0900

    add ランダムに並べる

--- src/plugins/return-the-hand/extend/component/hand-storage/hand-storage.component.ts
+++ src/plugins/return-the-hand/extend/component/hand-storage/hand-storage.component.ts
@@ -26,6 +26,35 @@ export const returnHandCardContextMenu = (that) => {
             }
           })
         }
+      },
+      {
+        name: 'ランダムに並べる',
+        action: ()=> {
+          SoundEffect.play(PresetSound.cardDraw);
+          const owner = that.handStorage.owner;
+          const x = that.handStorage.location.x;
+          const y = that.handStorage.location.y;
+
+
+          // ボード上のカードを取得
+          const cloneArray = [...that._calcTopObjects(that.tabletopService.cards).map(({obj})=>obj)];
+
+          // ランダムに並べる
+          const result = cloneArray.reduce((_,cur,idx) => {
+            let rand = Math.floor(Math.random() * (idx + 1));
+            cloneArray[idx] = cloneArray[rand]
+            cloneArray[rand] = cur;
+            return cloneArray
+          })
+          result.forEach((card, i) => {
+            card.location.x = x + i * 55*2;
+            card.location.y = y;
+            card.update();
+            if(owner === Network.peer.userId && that.handStorage.isVirtualScreen){
+              setAutoSelfViewCard(card)
+            }
+          })
+        }
       }
     ]
 }
