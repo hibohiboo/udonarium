bc6b286b85d043aac8873a2121d28592b73d4184
hibohiboo <hibohiboo66+github@gmail.com>
2025-12-10 02:14:40 +0900

    add context

--- src/app/service/tabletop-action.service.ts
+++ src/app/service/tabletop-action.service.ts
@@ -15,8 +15,6 @@ import { TextNote } from '@udonarium/text-note';
 
 import { ContextMenuAction } from './context-menu.service';
 import { PointerCoordinate } from './pointer-device.service';
-import { getCreateHandStorageMenu } from 'src/plugins/hand-storage/extend/service/tabletop-action.service';
-import { getCounterboardStorageMenu } from 'src/plugins/add-counter-board/extend/service/tabletop-action.service';
 
 @Injectable({
   providedIn: 'root'
@@ -202,8 +200,6 @@ export class TabletopActionService {
       this.getCreateTextNoteMenu(position),
       this.getCreateTrumpMenu(position),
       this.getCreateDiceSymbolMenu(position),
-      ...getCreateHandStorageMenu(position),
-      ...getCounterboardStorageMenu()
     ];
   }
 
