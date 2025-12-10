ee4c6850ffb291972abc83df95463d4e827e3d8d
hibohiboo <hibohiboo66+github@gmail.com>
2025-12-11 08:44:31 +0900

    fix 一時保存

---
+++ src/plugins/extends/class/game-character.ts
@@ -0,0 +1,5 @@
+import { initRotateOfCharacter } from "src/plugins/object-rotate-off/extends/class/game-character";
+
+export const extendGameCharacter = (that:any)=>{
+  initRotateOfCharacter(that);
+}
