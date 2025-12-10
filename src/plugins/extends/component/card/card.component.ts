6f351d1176dbffea334d43d9f78830c1e16221d6
hibohiboo <hibohiboo66+github@gmail.com>
2025-12-10 16:04:40 +0900

    fix card

--- src/plugins/extends/component/card/card.component.ts
+++ src/plugins/extends/component/card/card.component.ts
@@ -5,23 +5,6 @@ export const extendsCardComponent = (that: any) => {
   // keyboard-shortcut プラグインの初期化
   initKeyboardShortcutCard(that);
 
-  // tabIndex プロパティをプラグインから注入
-  // @HostBinding('tabIndex')相当の処理
-  Object.defineProperty(that, 'tabIndex', {
-    get: function() {
-      return this._tabIndex || '0';
-    },
-    set: function(value: string) {
-      this._tabIndex = value;
-      // DOM要素のtabindex属性を更新
-      if (this.elementRef && this.elementRef.nativeElement) {
-        this.elementRef.nativeElement.setAttribute('tabindex', value);
-      }
-    },
-    enumerable: true,
-    configurable: true
-  });
-
   // Angularのライフサイクルフックをプロトタイプレベルでオーバーライド
   const constructor = that.constructor;
   const originalNgAfterViewInit = constructor.prototype.ngAfterViewInit;
--- src/plugins/keyboard-shortcut/extend/component/card/card.component.ts
+++ src/plugins/keyboard-shortcut/extend/component/card/card.component.ts
@@ -70,6 +70,24 @@ export const onKeyDownKeyboardShortcutCard = (that, e: KeyboardEvent) => {
   }
 };
 
+/**
+ * TabIndexを付与。これをしないとオンマウスでフォーカスできないのでコンポーネントに対するキーイベントを取得できない。
+ */
 export const initKeyboardShortcutCard = (that) => {
-  that.tabIndex = '0'; //TabIndexを付与。これをしないとフォーカスできないのでコンポーネントに対するキーイベントを取得できない。
+  // tabIndex プロパティをプラグインから注入
+  // @HostBinding('tabIndex')相当の処理
+  Object.defineProperty(that, 'tabIndex', {
+    get: function() {
+      return this._tabIndex || '0';
+    },
+    set: function(value: string) {
+      this._tabIndex = value;
+      // DOM要素のtabindex属性を更新
+      if (this.elementRef && this.elementRef.nativeElement) {
+        this.elementRef.nativeElement.setAttribute('tabindex', value);
+      }
+    },
+    enumerable: true,
+    configurable: true
+  });
 };
