7947bdbb81db9bae13f8e3dd51d24d07b1f6079d
hibohiboo <hibohiboo66+github@gmail.com>
2026-03-31 23:31:17 +0900

    fix 不要ファイル削除

--- bin/container_build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# このシェルスクリプトのディレクトリの絶対パスを取得。
-bin_dir=$(cd $(dirname $0) && pwd)
-parent_dir=$bin_dir/..
-cp $parent_dir/package.json $parent_dir/docker/node/package.json
-cd $parent_dir/docker && docker-compose build
